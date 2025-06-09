156f05ad4b216eb080e3500750bb05dfd78f54e1
Liam <108475958+IOwnSpaceX@users.noreply.github.com>
2025-06-08 22:51:35 -0500

    Add files via upload

--- liam-jail/server.lua
+++ liam-jail/server.lua
@@ -1,8 +1,34 @@
 -- Jail event handler
 RegisterNetEvent("Liam:JailPlayerServer")
-AddEventHandler("Liam:JailPlayerServer", function(targetId, jailtime)
+AddEventHandler("Liam:JailPlayerServer", function(targetId, jailtime, jailReason)
     local targetPlayer = tonumber(targetId)
     if targetPlayer and GetPlayerName(targetPlayer) then
+        local playerName = GetPlayerName(targetPlayer)
+        local playerIdentifiers = GetPlayerIdentifiers(targetPlayer)
+        local discordId = nil
+        
+        for _, identifier in ipairs(playerIdentifiers) do
+            if string.find(identifier, "discord:") then
+                discordId = string.sub(identifier, 9)
+                break
+            end
+        end
+
+        if discordId then
+            local reason = string.format("%s. \nTime: %s seconds.", jailReason or "No reason provided", jailtime)
+            local moderatorName = GetPlayerName(source)
+            local moderatorDiscordId = nil
+
+            local moderatorIdentifiers = GetPlayerIdentifiers(source)
+            for _, identifier in ipairs(moderatorIdentifiers) do
+                if string.find(identifier, "discord:") then
+                    moderatorDiscordId = string.sub(identifier, 9)
+                    break
+                end
+            end
+            
+            Storage.addAction("~y~JAILED~w~~s~", discordId, reason, moderatorName, moderatorDiscordId)
+        end
         TriggerClientEvent("Liam:JailPlayer", targetPlayer, jailtime)
     end
 end)
